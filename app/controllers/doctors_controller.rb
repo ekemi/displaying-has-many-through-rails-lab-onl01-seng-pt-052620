6e0f15e30fa3df8b3159a3112bf61284c9f3db35
edmundo papi <edmundopierre@yahoo.com>
2020-11-17 01:04:18 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,12 +1,10 @@
-class DoctorsController < ApplicationController
+ef index
+    @doctors = Doctor.all
+  end
 
 
-def index
-  @doctors = Doctor.all
-end
-def show
-  @doctor = Doctor.find(params[:id])
-end
-
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
 
 end
