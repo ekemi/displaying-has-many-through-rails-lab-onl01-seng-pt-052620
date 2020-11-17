d543c96a13579ca8da515ef7f3adb8f16ffa1074
edmundo papi <edmundopierre@yahoo.com>
2020-11-17 01:00:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors
-  resources :patients, only:[:index,:show]
-  resources :appointments, only: [:show]
+  resources :doctors, :patients, only: [:index, :show]
+ resources :appointments, only: [:show]
 end
